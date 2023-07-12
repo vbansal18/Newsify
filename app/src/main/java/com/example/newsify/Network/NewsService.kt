6d776300b8e28a68b15fa20fb34c33a52e3e3b74
vbansal18 <bansal.learn@gmail.com>
2023-07-12 16:14:19 +0530

    api_key changed

--- app/src/main/java/com/example/newsify/Network/NewsService.kt
+++ app/src/main/java/com/example/newsify/Network/NewsService.kt
@@ -10,7 +10,8 @@ import retrofit2.http.Query
 //https://newsapi.org/v2/everything?excludeDomains=google.com&q=tech%20OR%20business%20OR%20apple%20OR%20politics&pageSize=10&apiKey=3b85bf1206fe4f85a5ee506ada712898&page=1&to=2023-07-11
 const val BASE_URL = "https://newsapi.org"
 const val API_KEY_ = "3ee56bd870144c0ca0a0783091596a4d"
-const val API_KEY = "3b85bf1206fe4f85a5ee506ada712898"
+const val API_KEY__ = "3b85bf1206fe4f85a5ee506ada712898"
+const val API_KEY = "2eb60f0e77df47398634909d96812e04"
 
 interface NewsInterface {
     @GET("/v2/everything?excludeDomains=google.com&q=tech%20OR%20business%20OR%20apple%20OR%20politics&pageSize=10&apiKey=$API_KEY")
